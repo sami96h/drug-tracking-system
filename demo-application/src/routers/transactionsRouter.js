bbd870994b115712335576ff01a55f8b27f11678
sami <same-96@hotmail.com>
2023-01-05 00:33:36 +0200

    transactions router

---
+++ demo-application/src/routers/transactionsRouter.js
@@ -0,0 +1,11 @@
+const { Router } =require ('express')
+
+const getTransactionsByUser = require('../controllers/getTransactionsByUser')
+const router = Router();
+
+
+router.get('/',getTransactionsByUser)
+
+
+
+module.exports=router
