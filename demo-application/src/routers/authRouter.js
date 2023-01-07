3bd8d06991359deca7c1edbb6420cb489247c597
sami <same-96@hotmail.com>
2023-01-07 23:08:12 +0200

    add register router

--- demo-application/src/routers/authRouter.js
+++ demo-application/src/routers/authRouter.js
@@ -1,8 +1,10 @@
-const login =require('../controllers/login')
+const authUser= require('../middleWares/authUser')
+const UserController = require('../controllers/UserController')
 const { Router } =require('express')
 
 const router = Router();
 
-router.post('/login', login);
+router.post('/login', UserController.login);
+router.post('/register',authUser,UserController.registerUser)
 
 module.exports= router;
